01e02ee0c458976c6029cb2dc39e6d5904afc9a9
Ana Mickaelly <ana.codes2@gmail.com>
2023-03-22 00:40:35 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['tasks'])) {
+    $_SESSION['tasks']  =  array();
+}
+
+if (isset($_GET['task_name']) ) {
+    array_push($_SESSION['tasks'], $_GET['task_name']);
+    unset($_GET['task_name']);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,600;0,800;1,400&family=Roboto+Slab:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    <title>Task Manager</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Task Manager</h1>
+        </div>
+        <div class="form">
+            <form action="" method="get">
+                <label for="task_name">Tarefa:</label>
+                <input type="text" name="task_name" placeholder="Nome da Tarefa">   
+                <button type="submit">Cadastrar</button>
+
+            </form>
+        </div>
+        <div class="separator"></div>
+        <div class="list-tasks">
+            <?php
+            if (isset($_SESSION['tasks'])) {
+                echo "<ul>";
+
+                foreach ($_SESSION['tasks'] as $key => $task) {
+                    echo "<li>$task</li>";
+                }
+
+                echo "</ul>";
+            }
+            ?>
+
+            <form action="" method="get">
+                <input type="hidden" name="clear" value="clear">
+                <button type="submit">Limpar Tarefas</button>
+            </form>
+        </div>
+        <div class="footer">
+            <p>Desenvolvido por Ana Mickaelly</p>
+        </div>
+    </div>
+</body>
+
+</html>
